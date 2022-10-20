f7e6597f34c713524f6c5c3b1336d4cd427534d8
qicoder <qhf20171002221@cug.edu.cn>
2022-10-20 16:42:35 +0800

    update 2022/10/20 16:45

---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,13 @@
+package com.hmdp.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 2021229100@tju.edu.cn
+ * @date 2022/10/20 16:36
+ */
+@Slf4j
+@Component
+public class CacheClient {
+}
